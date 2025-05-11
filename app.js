5fc4be9249e3b2f323c8d67aea1bcf21bd973317
mohtishim <mohtishim5@gmail.com>
2025-05-11 20:25:06 +0500

    ff

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCb-b42nrb4sFOOk25vIaEeD4Nmch420F0",
+    authDomain: "todo-database-bff47.firebaseapp.com",
+    databaseURL: "https://todo-database-bff47-default-rtdb.firebaseio.com",
+    projectId: "todo-database-bff47",
+    storageBucket: "todo-database-bff47.firebasestorage.app",
+    messagingSenderId: "379056051385",
+    appId: "1:379056051385:web:a4286071de59a87c6cf768"
+  };
+
+  // Initialize Firebase
+  var app = firebase.initializeApp(firebaseConfig);
+
 var paraElement = document.getElementById('para')
 var para = 0;
 
@@ -20,19 +33,19 @@ var inputElement = document.getElementById('input');
 var olDiv = document.getElementById('list');
 var editbtnElement;
 
-function save(){
-    if(inputElement.value === ""){
-        alert("Please Enter To Do list");
-    }
-    else{
+
+
+
+firebase.database().ref("todos").on("child_added", function(data){
         var listElement= document.createElement('li');
-        var listText = document.createTextNode(inputElement.value);
+        var listText = document.createTextNode(data.val().input_value);
         listElement.appendChild(listText);
 
         var deletebtnElement = document.createElement('button');
         var deletebtnText = document.createTextNode('Delete');
         deletebtnElement.setAttribute('class','dltBtn');
         deletebtnElement.setAttribute('onclick','deleteItem(this)');
+        deletebtnElement.setAttribute("id", data.val().id);
         deletebtnElement.appendChild(deletebtnText);
         listElement.appendChild(deletebtnElement)
 
@@ -40,25 +53,48 @@ function save(){
         var editbtnText = document.createTextNode('Edit');
         editbtnElement.setAttribute('class','editBtn');
         editbtnElement.setAttribute('onclick','edit(this)');
+        editbtnElement.setAttribute("id", data.val().id);
         editbtnElement.appendChild(editbtnText);
         listElement.appendChild(editbtnElement);
 
         olDiv.prepend(listElement);
         inputElement.value = ""
+    console.log(data.val().id);
+})
+
+function save(){
+    if(inputElement.value === ""){
+        alert("Please Enter To Do list");
+    }
+    else{
+        var id = firebase.database().ref("todos").push().key;
+        var obj = {
+            input_value:inputElement.value,
+            id:id,
+        }
+        
+        firebase.database().ref(`todos/${id}`).set(obj);
+        console.log(obj.id);
+
+
     }
 }
 
 function clearAll(){
     olDiv.innerHTML = "";
+    firebase.database().ref(`todos`).remove();
+
 }
 
 function deleteItem(a){
     a.parentElement.remove();
+    firebase.database().ref(`todos/${a.id}`).remove();
+    console.log(a.id);
 }
 
 function edit(b){
     var inputField = document.createElement('input');
-    inputField.setAttribute('id','input2');
+    inputField.setAttribute('class','input2');
 
     var okElement = document.createElement('button');
     var okText = document.createTextNode('Ok');
@@ -83,7 +119,18 @@ function okk(c){
     }
     else{
         c.parentElement.firstChild.nodeValue = input.value;
-        input.remove();
+        
+
+        var obj = {
+            input_value: input.value,
+            id: c.id,
+          };
+
+          console.log(obj.id);
+        
+          firebase.database().ref(`todos/${c.parentElement.firstChild.id}`).set(obj);
+
+          input.remove();
         c.remove();
 
         var allEditBtns = document.getElementsByClassName('editBtn');
